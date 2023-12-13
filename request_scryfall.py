2e77d8f5984222e9a189715e89a6cb19161003f2
Edmund Prescott <78518690+Edmund-Prescott@users.noreply.github.com>
2023-12-12 19:50:29 -0800

    Database working (as intended)
    fore·shad·ow
    /fôrˈSHadō,fərˈSHadō/
    verb
    gerund or present participle: foreshadowing
    be a warning or indication of (a future event).
    "it foreshadowed my preoccupation with jazz"

--- request_scryfall.py
+++ request_scryfall.py
@@ -1,15 +1,9 @@
-import json, sys
+import json
 import requests
 from config import DB_CONFIG as config
 from sqlalchemy.orm import declarative_base
 
-from sqlalchemy import (
-    create_engine,
-    Column,
-    JSON,
-    MetaData,
-    String,
-)
+from sqlalchemy import create_engine, Column, JSON, MetaData, VARCHAR
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -22,11 +16,24 @@ Base = declarative_base(metadata=MetaData(schema="cards_json"))  # Specify the s
 
 class Card(Base):
     __tablename__ = "cards"
-    id = Column(String(50), primary_key=True)
+    id = Column(VARCHAR(50), primary_key=True)
     response_data = Column(JSON)
 
 
-def get_cards(card_names):
+def create_session():
+    # Create the SQLAlchemy engine
+    engine = create_engine(DATABASE_URL, echo=True)
+
+    # Create the table if it doesn't exist
+    Base.metadata.create_all(bind=engine)
+
+    # Create a session
+    session = sessionmaker(bind=engine)()
+
+    return session
+
+
+def fetch_cards(card_names):
     # Scryfall API endpoint for card information
     api_url = "https://api.scryfall.com/cards/named"
 
@@ -34,75 +41,59 @@ def get_cards(card_names):
 
     # Make batch API request for multiple cards
     for card_name in card_names:
-        params = {"exact": card_name}
-        response = requests.get(api_url, params=params)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            card_data = response.json()
-
-            database_action(save_card, card_data)
+        card_data = read_card(card_name)
+        if card_data is not None:
+            results.append(card_data)
+            print(f"Fetched {card_name} from database successfully!")
 
         else:
-            # If the request was not successful, print an error message
-            print(f"Error: {response.status_code}")
+            params = {"exact": card_name}
+            response = requests.get(api_url, params=params)
+
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                card_data = response.json()
+
+                save_card(card_data)
+                print(f"Fetched {card_name} from Scryfall successfully!")
+                results.append(card_data)
+
+            else:
+                # If the request was not successful, print an error message
+                print(f"Error: {response.status_code}")
 
     return results
 
 
-def save_card(session, json_data):
+def save_card(json_data):
+    session = create_session()
+
     try:
         # Insert a row into the "cards" table with JSON data
         new_card = Card(id=json_data["name"], response_data=json.dumps(json_data))
         session.add(new_card)
         session.commit()
-        print("Card saved successfully!")
+        print(f'{json_data["name"]} saved successfully!')
     except Exception as e:
-        # Rollback in case of an error
-        session.rollback()
         print(f"Failed to save card. Error: {e}")
     finally:
         # Close the session
         session.close()
 
 
-def read_card(session, name):
+def read_card(name):
+    session = create_session()
+
     try:
         card = session.query(Card).filter_by(id=name).first()
-        print("Card read successfully!")
-        return json.loads(card)
+        print(f"Read {name} successfully!")
+        session.commit()
+        return card.response_data
     except Exception as e:
-        # Rollback in case of an error
-        session.rollback()
-        print(f"Failed to read card. Error: {e}")
+        print(f"! Failed to read card. Error: {e}")
     finally:
         # Close the session
         session.close()
 
 
-def database_action(function, data=None):
-    try:
-        # Create the SQLAlchemy engine
-        engine = create_engine(DATABASE_URL, echo=True)
-
-        # Create the table if it doesn't exist
-        Base.metadata.create_all(bind=engine)
-
-        # Create a session
-        session = sessionmaker(bind=engine)()
-
-        # Execute action on database
-        if data is None:
-            function(session)
-        else:
-            function(session, data)
-
-    except Exception as e:
-        print(f"Failed to connect to the database. Error: {e}")
-    finally:
-        session.close()
-        print("Session closed")
-
-
-# get_cards(["Forest"])
-database_action(read_card)
+fetch_cards(["Forest", "Mountain"])
